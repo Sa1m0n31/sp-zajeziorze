95a46bc97fbebfaee99556235862328b8418ad05
Szymon Burak <sajmon0031@gmail.com>
2023-08-24 15:24:36 +0200

    Finish mobile version of homepage - website ready for WordPress conversion

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ const mainCarousel = new Siema({
     selector: '.mainCarousel',
     perPage: 1,
     loop: true,
-    draggable: false
+    draggable: window.innerWidth < 768,
+    onChange: () => {
+        changeMainCarouselDotsView(mainCarousel.currentSlide)
+    }
 });
 const mainCarouselButtonsInner = Array.from(document.querySelectorAll('.hero__indicators--main .hero__indicators__item__inner'));
 
 const mainCarouselGoTo = (n) => {
     mainCarousel.goTo(n);
+}
 
+const changeMainCarouselDotsView = (n) => {
     mainCarouselButtonsInner.forEach((item, index) => {
         if(index === n) {
             item.style.opacity = '1';
@@ -23,7 +28,7 @@ const newsCarousel = new Siema({
     selector: '.newsCarousel',
     perPage: 1,
     loop: true,
-    draggable: false
+    draggable: window.innerWidth < 768
 });
 const newsCarouselButtonsInner = Array.from(document.querySelectorAll('.hero__indicators--news .hero__indicators__item__inner'));
 
@@ -51,4 +56,27 @@ const newsCarouselNext = () => {
 const newsCarouselGoTo = (n) => {
     newsCarousel.goTo(n);
     changeNewsCarouselDotsView(n);
+}
+
+const mobileMenu = document.querySelector('.mobileMenu');
+const mobileMenuChildren = Array.from(document.querySelectorAll('.mobileMenu>*'));
+
+const openMobileMenu = () => {
+    mobileMenu.style.transform = 'scaleX(1)';
+
+    setTimeout(() => {
+        mobileMenuChildren.forEach((item) => {
+            item.style.opacity = '1';
+        });
+    }, 200);
+}
+
+const closeMobileMenu = () => {
+    mobileMenuChildren.forEach((item) => {
+        item.style.opacity = '0';
+    });
+
+    setTimeout(() => {
+        mobileMenu.style.transform = 'scaleX(0)';
+    }, 200);
 }
